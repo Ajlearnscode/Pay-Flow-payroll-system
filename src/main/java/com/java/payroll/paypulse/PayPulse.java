753465cf9967d8ba41df5b40c7393fe9b0d5f88c
Asher Maxwell <ashermaxwellprogramming@gmail.com>
2025-11-20 09:07:47 -0500

    Initial Commit

---
+++ src/main/java/com/java/payroll/paypulse/PayPulse.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.java.payroll.paypulse;
+
+import com.java.payroll.view.Login;
+
+/**
+ *
+ * @author school
+ */
+public class PayPulse {
+
+    public static void main(String[] args) {
+        new Login().setVisible(true);
+    }
+}
